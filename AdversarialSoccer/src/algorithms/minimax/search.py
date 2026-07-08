96ecaedd1897b2a824b034747bbb405a73337131
jahdezg <ja.hernandezg12@uniandes.edu.co>
2026-07-08 08:47:30 -0500

    minimax

--- AdversarialSoccer/src/algorithms/minimax/search.py
+++ AdversarialSoccer/src/algorithms/minimax/search.py
@@ -39,12 +39,31 @@ def minimax_search(
         if is_cutoff(state, depth):
             return None, None, evaluation_function(state)
 
+        #Acciones colombia y el rival
         colombia_actions = legal_actions(state, Team.COLOMBIA)
         rival_actions = legal_actions(state, Team.RIVAL)
 
-        ### YOUR CODE HERE ###
-        # --- SOLUTION START ---
+        # Aquí las mejores opciones que encuentre Colombia
+        posibles_escenarios = []
 
-        # --- SOLUTION END ---
+        for colombia_action in colombia_actions:
+            
+            #respuestas posibles del rival
+            respuestas_rival = []
+            for rival_action in rival_actions:
+                successor = step(state, colombia_action, rival_action)
+                _, _, value = ply(successor, depth - 1)
+                
+                #Se guarda la accion
+                respuestas_rival.append((rival_action, value))
+
+            #El rival minimiza
+            worst_rival_action, worst_value = min(respuestas_rival, key=lambda item: item[1])
+            posibles_escenarios.append((colombia_action, worst_rival_action, worst_value))
+
+        #Colombia maximiza
+        best_colombia_action, best_rival_action, best_value = max(posibles_escenarios, key=lambda item: item[2])
+
+        return best_colombia_action, best_rival_action, best_value
 
     return finish_search_root(*ply(state, depth))
